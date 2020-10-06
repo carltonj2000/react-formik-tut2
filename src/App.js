59aad62161e96c3f1dcc68866add7248fed0f89a
Carlton Joseph <carlton.joseph@gmail.com>
2020-10-06 12:58:04 -0700

    Add enrollment form example

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from "react";
 import "./App.css";
 
 // import YoutubeForm from "./components/YoutubeForm"; // use for video 1-30
-// import FormikContainer from "./components/FormikContainer"; // vid 31-39
-//import LoginForm from "./components/LoginForm"; // vid 40
-import RegistrationForm from "./components/RegistrationForm";
+// import FormikContainer from "./components/FormikContainer"; // vid 31-38
+// import LoginForm from "./components/LoginForm"; // vid 39
+// import RegistrationForm from "./components/RegistrationForm"; // vid 40
+import CourseEnrollmentForm from "./components/CourseEnrollmentForm";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
-      {/* <LoginForm /> Used for video 40 */}
-      {/* <FormikContainer /> Used for videos 31-39 - form components */}
+      <CourseEnrollmentForm />
+      {/* <RegistrationForm /> Used for video 40 */}
+      {/* <LoginForm /> Used for video 39 */}
+      {/* <FormikContainer /> Used for videos 31-38 - form components */}
       {/* <YoutubeForm /> Used for videos 1-30 */}
     </div>
   );
---
+++ src/components/CourseEnrollmentForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import FormikControls from "./FormikControls";
+
+function CourseEnrollmentForm() {
+  const initialValues = {
+    email: "",
+    bio: "",
+    course: "",
+    skills: [],
+    startDate: null,
+  };
+
+  const courseOptions = [
+    { value: "", text: "Select a course" },
+    { value: "english", text: "English" },
+    { value: "french", text: "French" },
+    { value: "spanish", text: "Spanish" },
+  ];
+
+  const skillOptions = [
+    { value: "html", text: "HTML" },
+    { value: "css", text: "CSS" },
+    { value: "javascript", text: "JavaScript" },
+  ];
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required().email(),
+    bio: Yup.string().required(),
+    course: Yup.string().required(),
+    startDate: Yup.date().required().nullable(),
+  });
+
+  const onSubmit = (values) => console.log("Form data", values);
+
+  return (
+    <Formik {...{ initialValues, validationSchema, onSubmit }}>
+      {(formik) => (
+        <Form>
+          <FormikControls
+            control="input"
+            type="email"
+            name="email"
+            label="Email"
+          />
+          <FormikControls control="textarea" name="bio" label="Bio" />
+          <FormikControls
+            control="select"
+            name="course"
+            label="Course"
+            options={courseOptions}
+          />
+          <FormikControls
+            control="checkbox"
+            name="skills"
+            label="Skills"
+            options={skillOptions}
+          />
+          <FormikControls control="date" name="startDate" label="Course Date" />
+          <button type="submit" disabled={!formik.isValid}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default CourseEnrollmentForm;
